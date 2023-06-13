c0e12b82bea61e85f710ab5ebc3f29e904397929
Lucianodcr <lucianodelcapellan@gmail.com>
2023-06-12 23:04:50 -0300

    prueba

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ItemListContainer from './ItemListContainer/ItemListContainer'
+
+const App = () => {
+  return (
+    <div>
+      <ItemListContainer productos={"productos"}/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/ItemListContainer/ItemListContainer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ItemListContainer = ({productos}) => {
+  return (
+    <div>
+        {productos}
+    </div>
+  )
+}
+
+export default ItemListContainer
